52da91f62521af0be68c7731536ec6b06e3803e9
ChauveSouris <desseobscurite@gmail.com>
2023-05-20 19:12:26 +0300

    Added AlertControler for warrning

--- TestApp/FirstViewController.swift
+++ TestApp/FirstViewController.swift
@@ -21,22 +21,36 @@ final class FirstViewController: UIViewController {
     
     
     // MARK: - Outlet
-    
     @IBAction func loginInButton() {
     }
     
-
+    @IBAction func loginButton() {
+        suggestsAlert(forTextField: "Login:", andMeaning: "")
+    }
+    
+    @IBAction func passwordButton() {
+        suggestsAlert(forTextField: "Password:", andMeaning: "")
+    }
+    
 }
 
 // MARK: - UIAlertController
 extension FirstViewController {
     private func suggestsAlert(forTextField text: String, andMeaning meaning: String) {
         let alert = UIAlertController(title: text, message: meaning, preferredStyle: .alert)
-        let action = UIAlertAction(title: "CLOSE", style: .default) { _ in
-            self.paswordTF.text = ""
-        }
+        let action = UIAlertAction(title: "OK", style: .default)
         alert.addAction(action)
         present(alert, animated: true)
     }
 }
 
+extension FirstViewController {
+    private func warrningAlert(forTitle title: String, andText text: String) {
+        let warningAlert = UIAlertController(title: title, message: text, preferredStyle: .alert)
+        let action = UIAlertAction(title: "CLOSE", style: .destructive) { _ in
+            self.paswordTF.text = ""
+        }
+        warningAlert.addAction(action)
+        present(warningAlert, animated: true)
+    }
+}
