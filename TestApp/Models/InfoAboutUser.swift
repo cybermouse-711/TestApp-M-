e8f30fe1436c8f3f1816622e027f5a06fed50a2a
ChauveSouris <desseobscurite@gmail.com>
2023-05-23 00:03:23 +0300

    Created Model User

---
+++ TestApp/Models/InfoAboutUser.swift
@@ -0,0 +1,19 @@
+//
+//  InfoAboutUser.swift
+//  TestApp
+//
+//  Created by Елизавета Медведева on 22.05.2023.
+//
+
+
+struct User {
+    let loginUser: String
+    let passwordUser: String
+    let person: Person
+    
+}
+
+struct Person {
+    let infoAboutPerson: String 
+    
+}
