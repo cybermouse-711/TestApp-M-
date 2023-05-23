936279fcc23aad13bcf3f89ca7e2a93ce23a5bbc
ChauveSouris <desseobscurite@gmail.com>
2023-05-23 23:10:15 +0300

    Created Struct

--- TestApp/Models/InfoAboutUser.swift
+++ TestApp/Models/InfoAboutUser.swift
@@ -7,12 +7,32 @@
 
 
 struct User {
-    let loginUser = "Liza"
-    let passwordUser = "123456"
+    let loginUser: String
+    let passwordUser: String
     let person: Person
+    
+    static func showsDataUser() -> User {
+        User(
+            loginUser: "Liza",
+             passwordUser: "123456",
+             person: Person(
+                name: "Елизавета",
+                surname: "Медведева",
+                company: "Forma",
+                job: "Инженер-строитель",
+                info: "Я проживаю в Москве. Работаю инженером в жилищном строительстве. Моя компания реализует объекты, которые вписываются в городской ландшафт и не загрязняют окружающую среду. Также у меня есть собака породы Немецкий пинчер. А в свободное от работы время, я люблю гулять в парках и читать научпоп о космосе."
+             )
+        )
+    }
 }
 
 struct Person {
-    let infoAboutPerson: String
     
-}
+    let name: String
+    let surname: String
+    let company: String
+    let job: String
+    let info: String
+ }
+
+
